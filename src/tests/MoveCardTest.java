c7a480d7a8cc1567d3d53360b5275d238f6381e5
lozanoco <x100714@PR07430X100714P.ad.produbanes.corp>
2015-04-14 11:32:41 +0200

    Define Test for move waste --> Foundation

--- src/tests/MoveCardTest.java
+++ src/tests/MoveCardTest.java
@@ -2,6 +2,30 @@ package tests;
 
 //Pruebas para Mover: Baraja-descarte, descarte-palo, descarte-trabajo
 
+import static org.junit.Assert.*;
+import game.MoveCardController;
+import java.util.Stack;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MoveCardTest {
+	
+	private MoveCardController moveCardController;
+	
+	@Test
+	public void moveFromWasteToFoundationTest(){
+		//No cards in waste
+		
+		//No cards in foundation & A in waste
+		
+		//No cards in foundation & no A in waste
+		
+		//Waste card consecutive to Foundation card
+		
+		//Waste card no consecutive to Foundation card
+		
+		//Waste card consecutive to Foundation card but different type
+		
+	}
 
 }
--- src/tests/StartGameTest.java
+++ src/tests/StartGameTest.java
@@ -1,5 +1,10 @@
 package tests;
 
+import static org.junit.Assert.*;
+import java.util.Stack;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StartGameTest {
 
 }
